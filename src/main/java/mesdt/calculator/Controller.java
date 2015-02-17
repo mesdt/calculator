e8ddffa58764b61b8494dd46831cab1f14388484
Sasha Go <a@ptx.su>
2015-02-18 00:39:28 +0600

    CalculatorController -> Controller

--- src/main/java/mesdt/calculator/Controller.java
+++ src/main/java/mesdt/calculator/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CalculatorController {
+public class Controller {
 
 	@Autowired
 	private Svc svc;
